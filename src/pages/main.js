02733878fbe769821ba8a940f3b5965558a9d024
Daniel Lucas <daniel.camargo@dokoro.com.br>
2019-11-13 13:40:02 -0300

    Estilizando o APP

--- src/pages/main.js
+++ src/pages/main.js
@@ -32,12 +32,12 @@ export default class Main extends Component {
     }
 
     renderItem = ({ item }) => (
-        <View>
-            <Text>{ item.title }</Text>
-            <Text>{ item.description }</Text>
+        <View style={ styles.productContainer }>
+            <Text style={ styles.productTitle }>{ item.title }</Text>
+            <Text style={ styles.productDescription }>{ item.description }</Text>
 
-            <TouchableOpacity onPress={ () => {} }>
-                <Text>Acessar</Text>
+            <TouchableOpacity style={ styles.productButton } onPress={ () => {} }>
+                <Text style={ styles.productButtonText }>Acessar</Text>
             </TouchableOpacity>
         </View>
     )
@@ -46,6 +46,7 @@ export default class Main extends Component {
         return (
             <View style={ styles.container }>
                 <FlatList
+                    contentContainerStyle={ styles.list }
                     data={ this.state.products }
                     keyExtractor={ item => item._id }
                     renderItem={ this.renderItem }
@@ -57,6 +58,50 @@ export default class Main extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#FAFAFA'
+    },
+
+    list: {
+        padding: 20
+    },
+
+    productContainer: {
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20
+    },
+
+    productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+
+    productDescription: {
+        fontSize: 16,
+        color: '#999',
+        marginTop: 4,
+        lineHeight: 24
+    },
+
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#DA552F',
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
+    },
+
+    productButtonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#DA552F'
     }
 })
